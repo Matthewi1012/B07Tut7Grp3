39b726c4f07fd500fcaa138c3efd6b7598b9c964
Kevin Li <kevinc.li@mail.utoronto.ca>
2022-11-26 06:34:35 -0500

    Removed "unneeded" files, added utscStudent constructor to get an existing student, completed updateCourses module in Student

--- app/src/main/java/com/example/b07tut7grp3/Student.java
+++ app/src/main/java/com/example/b07tut7grp3/Student.java
@@ -1,12 +1,21 @@
 package com.example.b07tut7grp3;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.HashSet;
+import java.util.List;
 
 abstract class Student extends User{
     public abstract double getCreditsEarned();
     protected String firstName, lastName;
-    protected HashSet<String> coursesTaken;
-    protected HashSet<String> plannedCourses;
+    protected List<String> coursesTaken;
+    protected List<String> plannedCourses;
     protected int currentYear;
     protected String currentSchool;
 
@@ -23,15 +32,18 @@ abstract class Student extends User{
         }
         coursesTaken.add(completed);
         plannedCourses.remove(completed);
-        MessageSystem success = new MessageSystem("added course " + completed);
-        success.successMessage();
+        DatabaseReference dbref = FirebaseDatabase.getInstance().getReference()
+                .getRoot().child("Users").child("Students");
+        updateCourses(dbref);
+
     }
+    abstract void updateCourses(DatabaseReference dbref);
     // Getter methods
     public int getCurrentYear(){
         return currentYear;
     }
-    public HashSet<String> getCoursesTaken(){ return coursesTaken; }
-    public HashSet<String> getPlannedCourses(){ return plannedCourses; }
+    public List<String> getCoursesTaken(){ return coursesTaken; }
+    public List<String> getPlannedCourses(){ return plannedCourses; }
     public String getFirstName() { return firstName; }
     public String getLastName() { return lastName; }
     public String getCurrentSchool(){
--- app/src/main/java/com/example/b07tut7grp3/utscStudent.java
+++ app/src/main/java/com/example/b07tut7grp3/utscStudent.java
@@ -3,31 +3,70 @@ package com.example.b07tut7grp3;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 
 public class utscStudent extends Student{
     protected Subject currentPOSt;
-    public utscStudent(String firstName, String lastName, HashSet<String> coursesTaken,
+    public utscStudent(String firstName, String lastName, List<String> coursesTaken,
                    int currentYear, String currentSchool, Subject post,
                        String email, String username){
         this.firstName = firstName;
         this.lastName = lastName;
         this.coursesTaken = coursesTaken;
-        this.plannedCourses = new HashSet<>();
+        this.plannedCourses = new ArrayList<>();
         this.currentYear = currentYear;
         this.currentSchool = currentSchool;
         currentPOSt = post;
         this.email = email;
         this.username = username;
     }
-    public utscStudent(DatabaseReference dbref){
+    public utscStudent(DataSnapshot dbref) throws ExceptionMessage {
         //Gets an existing student
+        /*
+        To whoever uses this function
+        Add the following code:
+        Database dbref = FirebaseDatabase.getInstance()
+                .getReference().getRoot().child("Users").child("Students")
+                .child("utscStudents").child(whatever the username is);
+        dbref.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void OnComplete(@NonNull Task<DataSnapshot> task){
+                utscStudent student = new utscStudent(task.getResult());
+                // can add a success message or something here...
+            }
+        });
+        // continue code here...
+         */
+        try {
+            this.firstName = dbref.child("FirstName").getValue().toString();
+            this.lastName = dbref.child("LastName").getValue().toString();
+            this.username = dbref.getKey();
+            this.email = dbref.child("Email").getValue().toString();
+            this.currentSchool = "UTSC";
+            this.currentYear = Integer.parseInt(dbref.child("currentYear").getValue().toString());
+            this.currentPOSt = Subject.valueOf(dbref.child("POst").getValue().toString());
+            List<String> coursesTaken = new ArrayList<>();
+            List<String> plannedCourses = new ArrayList<>();
+            for(DataSnapshot i : dbref.child("coursesTaken").getChildren()){
+                coursesTaken.add((String)(i.getValue()));
+            }
+            for(DataSnapshot i : dbref.child("plannedCourses").getChildren()){
+                plannedCourses.add((String)(i.getValue()));
+            }
+            this.coursesTaken = coursesTaken;
+            this.plannedCourses = plannedCourses;
+        }
+        catch(NullPointerException e){
+            throw new ExceptionMessage("Could not find data!" + e.getMessage());
+        }
     }
     private void uploadData(){
         Map<String, Object> userMap = new HashMap<>();
@@ -54,6 +93,20 @@ public class utscStudent extends Student{
                 message.successMessage();
             }
         });
+    }
+    @Override
+    protected void updateCourses(DatabaseReference dbref){
+        dbref = dbref.child("utscStudents").child(username);
+        Map<String, Object> detailsMap = new HashMap<>();
+        String[] coursesTaken = new String[this.coursesTaken.size()];
+        this.coursesTaken.toArray(coursesTaken);
+        String[] plannedCourses = new String[this.plannedCourses.size()];
+        this.plannedCourses.toArray(plannedCourses);
+        detailsMap.put("plannedCourses", plannedCourses);
+        detailsMap.put("coursesTaken", coursesTaken);
+        dbref.updateChildren(detailsMap);
+        MessageSystem success = new MessageSystem("added course successfully!");
+        success.successMessage();
     }
     @Override
     public double getCreditsEarned() {
