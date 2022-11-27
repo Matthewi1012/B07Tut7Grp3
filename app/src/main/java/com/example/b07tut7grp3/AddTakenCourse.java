52ba16f7ab0793fa41248762e8f0db5fb14c805b
Matthew Iannantuono <miannantuono12@gmail.com>
2022-11-26 23:20:44 -0500

    added feature activities of student home page

---
+++ app/src/main/java/com/example/b07tut7grp3/AddTakenCourse.java
@@ -0,0 +1,14 @@
+package com.example.b07tut7grp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTakenCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_taken_course);
+    }
+}
---
+++ app/src/main/java/com/example/b07tut7grp3/StudentCoursesTaken.java
@@ -0,0 +1,14 @@
+package com.example.b07tut7grp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentCoursesTaken extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_courses_taken);
+    }
+}
