b62a19b86cf780a794f75db19e045c68edbdcb72
Brian <brianzhou03@hotmail.com>
2022-12-01 21:33:52 -0500

    updated ui for course planner

--- app/src/main/java/com/example/b07tut7grp3/student_add_planned_courses.java
+++ app/src/main/java/com/example/b07tut7grp3/student_add_planned_courses.java
@@ -2,6 +2,7 @@ package com.example.b07tut7grp3;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -9,7 +10,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -23,7 +26,13 @@ import java.util.List;
 public class student_add_planned_courses extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     List<String> courses;
     List<String> completed;
+    List<String> plannedCourses;
+    ArrayList<plannedCourse> planned;
     Spinner eligibleCourseList;
+    RecyclerView recyclerView;
+    Button AddCourseBtn;
+    Button remove;
+
     String uid = "EZOZ4QTpQLXCD8WkOxnhclkE0iO2";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,19 +41,30 @@ public class student_add_planned_courses extends AppCompatActivity implements Ad
 
         courses = new ArrayList<>();
         completed = new ArrayList<>();
+        plannedCourses = new ArrayList<>();
         eligibleCourseList = findViewById(R.id.coursesList);
+        planned = new ArrayList<>();
+        AddCourseBtn = findViewById(R.id.button);
+
+        plannedCourseAdapter courseArrayAdapter = new plannedCourseAdapter(R.layout.plannedcourses_row, planned);
+        recyclerView = (RecyclerView) findViewById(R.id.plannedCourseList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(courseArrayAdapter);
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, courses);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         eligibleCourseList.setAdapter(adapter);
         eligibleCourseList.setOnItemSelectedListener(this);
 
-
         DatabaseReference database = FirebaseDatabase.getInstance().getReference().getRoot().child("Courses");
         database.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 courses.clear();
+                completed.clear();
+                planned.clear();
+                plannedCourses.clear();
                 for (DataSnapshot snap: snapshot.getChildren()) {
                     courses.add(snap.getKey());
                 }
@@ -52,17 +72,17 @@ public class student_add_planned_courses extends AppCompatActivity implements Ad
                 database2.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        completed.clear();
                         for (DataSnapshot snap: snapshot.getChildren()) {
                             completed.add(snap.getValue(String.class));
                             //System.out.println(snap.getValue());
                         }
                         //System.out.println(completed.toString());
                         courses.removeAll(completed);
+
                         //System.out.println(courses.toString());
                         adapter.notifyDataSetChanged();
+                        courseArrayAdapter.notifyDataSetChanged();
                     }
-
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
 
@@ -75,12 +95,53 @@ public class student_add_planned_courses extends AppCompatActivity implements Ad
 
             }
         });
+
+        DatabaseReference database3 = FirebaseDatabase.getInstance().getReference().getRoot().child("Users").child("Students").child("utscStudents").child(uid).child("plannedCourses");
+        database3.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                planned.clear();
+                plannedCourses.clear();
+                for (DataSnapshot snap: snapshot.getChildren()) {
+                    plannedCourses.add(snap.getValue(String.class));
+                }
+                for(int i=0; i<plannedCourses.size(); i++) {
+                    planned.add(new plannedCourse(plannedCourses.get(i)));
+                }
+                if (plannedCourses.isEmpty()){
+                    planned.add(new plannedCourse("No Courses Added"));
+                }
+
+                adapter.notifyDataSetChanged();
+                courseArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+        AddCourseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(student_add_planned_courses.this, "Added Course", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        courseArrayAdapter.setOnItemClickListener(new plannedCourseAdapter.OnItemClickedListener() {
+            @Override
+            public void onItemClick(int position) {
+                Toast.makeText(student_add_planned_courses.this, "Test123", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
     }
+
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
